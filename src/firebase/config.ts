e01ed0558d3045530009195975a9cfcd4fa2700b
Ridd Rockman <riddhishslowmo@gmail.com>
2026-02-26 16:24:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4399073863-bb9ab",
+  "appId": "1:614531964004:web:a7908ed3fcb7b4a3001371",
+  "apiKey": "AIzaSyDI1__yrwr_bglKKVT8xQLjgs77FHPFQrk",
+  "authDomain": "studio-4399073863-bb9ab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "614531964004"
+};
